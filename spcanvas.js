4b9e6b9733cede8a291c85609c85c8111ca47028
Stijn Dekker <sdekker@sp.nl>
2017-03-06 13:22:41 +0100

    Final fixes.

--- spcanvas.js
+++ spcanvas.js
@@ -1,5 +1,4 @@
 var canvas;
-var canvasWidth;
 var ctx;
 var x;
 var y;
@@ -10,21 +9,28 @@ var textcolor;
 var lineHeight  = 190;
 var startHeight = 550;
 var lastY       = startHeight;
-var maxChars    = 50;
+var maxChars    = 65;
+var canvasWidth = 1750;
+var canvasHeight = 2475;
 
 var font  = '140pt HelveticaInseratLTPro';
 var align = 'center';
 
-var veranderbg1 = document.getElementById('thumb1');
-var veranderbg2 = document.getElementById('thumb2');
-var veranderbg3 = document.getElementById('thumb3');
-var veranderbg4 = document.getElementById('thumb4');
-var veranderbg5 = document.getElementById('thumb5');
-var veranderbg6 = document.getElementById('thumb6');
+$(window).load(function(){
 
-window.onload = function () {
   prepareExample();
-}
+
+  $('.thumb').on('click',function(){
+    var imgId = $(this).attr('data-bg');
+    img = document.getElementById(imgId);
+    textcolor = $(this).attr('data-textcolor'); 
+    changeBackground(); 
+  });
+
+  document.getElementById('checkboxAgreed').onchange = function () {
+    document.getElementById('download_container').className = this.checked;
+  };
+});
 
 function changeBackground() {
   ctx.fillStyle = textcolor;
@@ -32,7 +38,7 @@ function changeBackground() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(img, x, y);
   ctx.restore();
-  text          = document.getElementById('custom-text').value;
+  text = $('#custom-text').val();
   text = text.toUpperCase();
   var newHeight = startHeight - ((lastY - startHeight) / 2);
   wrapText(ctx, text, canvas.width / 2, newHeight, canvasWidth - canvasWidth / 15, lineHeight);
@@ -40,53 +46,14 @@ function changeBackground() {
 
 function prepareExample() {
   img = document.getElementById('bg1');
-  textcolor = '#ffe000';
-
-  veranderbg1.onclick = function () {
-    img = document.getElementById('bg1');
-    textcolor = '#ffe000';
-    changeBackground();
-  };
-
-  veranderbg2.onclick = function () {
-    img = document.getElementById('bg2');
-    textcolor = '#ea0000';
-    changeBackground();
-  };
-
-  veranderbg3.onclick = function () {
-    img = document.getElementById('bg3');
-    textcolor = '#ff4fa5';
-    changeBackground();
-  };
-
-  veranderbg4.onclick = function () {
-    img = document.getElementById('bg4');
-    textcolor = '#ff4fa5';
-    changeBackground();
-  };
-
-  veranderbg5.onclick = function () {
-    img = document.getElementById('bg5');
-    textcolor = '#ffe000';
-    changeBackground();
-  };
-
-  veranderbg6.onclick = function () {
-    img = document.getElementById('bg6');
-    textcolor = '#00e000';
-    changeBackground();
-  };
-
+  textcolor = $('.thumb:first').attr('data-textcolor');
 
   var deviceWidth = window.innerWidth;
-  canvasWidth     = 1750;
-  canvasHeight    = 2475;
-  canvas          = document.getElementById('memecanvas');
 
-
+  canvas        = document.getElementById('memecanvas');
   canvas.width  = canvasWidth;
   canvas.height = canvasHeight;
+
   ctx           = canvas.getContext('2d');
 
   x = canvas.width / 2 - img.width / 2;
@@ -100,15 +67,10 @@ function prepareExample() {
   ctx.fillStyle = textcolor;
   doTransform();
 
-  ctx.textAlign = align;
-  ctx.lineWidth = 0;
-  ctx.font      = font;
-//      ctx.textBaseline = 'top';
-
 }
 
 function doTransform() {
-  text  = document.getElementById('custom-text').value;
+  text  = $('#custom-text').val();
   text = text.toUpperCase();
   chars = document.getElementById('charsLeft');
 
@@ -120,6 +82,7 @@ function doTransform() {
 
     ctx.translate(canvas.width / 2, canvas.height / 2);
     ctx.translate(-canvas.width / 2, -canvas.height / 2);
+
     // Finally, draw the image
     ctx.drawImage(img, x, y);
 
@@ -139,7 +102,6 @@ function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
     var metrics   = ctx.measureText(testLine);
     var testWidth = metrics.width;
     if (testWidth > maxWidth && n > 0) {
-      //ctx.strokeText(line, x, y);
       ctx.fillText(line, x, y);
       line  = words[n] + ' ';
       y += lineHeight;
@@ -149,17 +111,12 @@ function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
       line = testLine;
     }
   }
-  //ctx.strokeText(line, x, y);
   ctx.fillText(line, x, y);
 }
 
 function downloadCanvasAsPng() {
   ReImg.fromCanvas(document.querySelector('canvas')).downloadPng();
 }
-
-document.getElementById('checkboxAgreed').onchange = function () {
-  document.getElementById('download_container').className = this.checked;
-};
 
 function onBlur(el) {
   if (el.value == '') {
